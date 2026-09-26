12f7c489c4bff58c7e063935d1b6874c1e0d2f14
Aleustim <aleustim@gmail.com>
2026-09-26 15:54:23 +0200

    Подготовить графику до начала партии и проверять загрузку без кеша

--- scripts/studio-wire.js
+++ scripts/studio-wire.js
@@ -3,6 +3,6 @@ const fs=require('node:fs');
 for(const id of process.argv.slice(2)){
  const file=id+'.html';let s=fs.readFileSync(file,'utf8');
  if(!s.includes('style-casual-studio.css'))s=s.replace('<script>window.ИграСтраницы=','<link rel="stylesheet" href="style-casual-studio.css?v=1"><script>window.ИграСтраницы=');
- if(!s.includes('js/casual-studio.js'))s=s.replace('<script src="js/casual-ui.js','<script src="js/casual-studio.js?v=1"></script><script src="js/casual-ui.js');
- s=s.replaceAll('premium-1','studio-1');fs.writeFileSync(file,s);
+ if(!s.includes('js/casual-studio.js'))s=s.replace('<script src="js/casual-ui.js','<script src="js/casual-studio.js?v=2"></script><script src="js/casual-ui.js');
+ s=s.replaceAll('premium-1','studio-1').replace('casual-studio.js?v=1','casual-studio.js?v=2').replace('casual-ui.js?v=studio-1','casual-ui.js?v=studio-2');fs.writeFileSync(file,s);
 }
